8c01a75cf1fed4c8b12d885214d7034ba2c65e30
KieSun <zx597813039@gmail.com>
2022-01-17 18:49:48 +0800

    demo

--- scrape_url/src/main.rs
+++ scrape_url/src/main.rs
@@ -1,16 +1,30 @@
 
+
 use std::fs;
+// main 函数现在返回一个 Result
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://www.rust-lang.org/";
+    let output = "rust.md";
 
-fn main() {
-  let url = "https://www.rust-lang.org/";
-  let output = "rust.md";
-  
-  println!("Fetching url: {}", url);
-  let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    println!("Fetching url: {}", url);
+    let body = reqwest::blocking::get(url)?.text()?;
 
-  println!("Converting html to markdown...");
-  let md = html2md::parse_html(&body);
+    println!("Converting html to markdown...");
+    let md = html2md::parse_html(&body);
 
-  fs::write(output, md.as_bytes()).unwrap();
-  println!("Converted markdown has been saved in {}.", output);
+    fs::write(output, md.as_bytes())?;
+    println!("Converted markdown has been saved in {}.", output);
+
+    Ok(())
+}
+
+fn fib_loop(n: u8) {
+  let (mut a, mut b) = (1, 1);
+
+  for _i in 2..n {
+    let c = a + b;
+    a = b;
+    b = c;
+    println!("next value is {}", b);
+  }
 }
